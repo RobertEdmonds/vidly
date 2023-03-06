333bdb5e9a7793369f4852dada09c3483a7e5591
Robert Edmonds <bobby.edmonds89@gmail.com>
2023-03-06 14:52:19 -0500

    Add docstring to admin

--- movies/admin.py
+++ movies/admin.py
@@ -1,11 +1,14 @@
+"""Allows tables to be accessed in admin portal"""
 from django.contrib import admin
 from .models import Genre, Movie
 # Register your models here.
 
 class GenreAdmin(admin.ModelAdmin):
+    """Adjust the view of genre table"""
     list_display = ("id", "name")
 
 class MovieAdmin(admin.ModelAdmin):
+    """Adjust the view of movie table"""
     exclude = ("date_created",)
     list_display = ("title", "release_year", "number_in_stock", "daily_rate")
 
