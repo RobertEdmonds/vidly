499294b3a1c2122ee1b8eabac2c895d054491e48
Robert Edmonds <bobby.edmonds89@gmail.com>
2023-03-08 13:01:55 -0500

    Make detail.html and connect to detail in views

--- movies/views.py
+++ movies/views.py
@@ -12,4 +12,5 @@ def index(request):
 
 def detail(request, movie_id):
     """To add value to home page"""
-    return HttpResponse(movie_id)
+    movie = Movie.objects.get(pk=movie_id)
+    return render(request, "movies/detail.html", {"movie": movie})
