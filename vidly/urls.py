0d10b2f9bde6d3b64d708a8b348fea964fc4c758
Robert Edmonds <bobby.edmonds89@gmail.com>
2023-03-10 10:47:12 -0500

    Create home page

--- vidly/urls.py
+++ vidly/urls.py
@@ -14,14 +14,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
-# pylint: disable = no-name-in-module
+# pylint: disable = no-name-in-module, no-member
 from django.contrib import admin
 from django.urls import path, include
 from api.models import MovieResource
+from . import views
 
 movie_resource = MovieResource()
 
 urlpatterns = [
+    path('', views.home),
     path('admin/', admin.site.urls),
     path('movies/', include('movies.urls')),
     path('api/', include(movie_resource.urls))
